69b725719444b1cfcdfc0cc7ed24b3ff0d7013e7
Enrique <enrique.sousa@gmail.com>
2025-10-28 19:01:40 -0700

    35. Explicación del directorio SRC

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // /hola mundo => HolaMundoComponent
+
+    // /usuarios => UsuariosComponent
+
+    //  /admin => AdminComponent
+
+    // / => HomeComponent
+
+    // /404 => NotFoundComponent
+
+];

e1998b67e60357adf88cddf0a6caae65dba8c1be
Enrique <enrique.sousa@gmail.com>
2025-10-29 08:49:56 -0700

    38. Señales

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,21 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
     templateUrl: './counter-page.html',
     styleUrl: './counter-page.css',
 })
 export class CounterPageComponent {
+
     counter = 10;
+    counterSignal = signal(10);
 
     increaseBy(value: number) {
         this.counter += value;
-    }
-
-    decrementBy(value: number) {
-        this.counter -= value;
+        // Mandamos llamar un callback function
+        this.counterSignal.update( (current) => current + value);
     }
 
     resetCounter() {
-        this.counter = 10;
+        this.counter = 0;
+        this.counterSignal.set(0);
     }
+
 }

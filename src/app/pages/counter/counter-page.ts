8fb0299d0dcb2ac4d5311d234f8b7baa7e1bee86
Enrique <enrique.sousa@gmail.com>
2025-10-29 09:51:11 -0700

    39. Zoneless Angular

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,13 +1,26 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
     templateUrl: './counter-page.html',
     styleUrl: './counter-page.css',
+
+    // Para quitar el zoneJs y solo trabajar con señales
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterPageComponent {
 
     counter = 10;
     counterSignal = signal(10);
+
+    constructor() {
+        // Cada dos segundos mandamos a consola un tick
+        setInterval( () => {
+            // this.counter += 1;
+            this.increaseBy(1);
+            // this.counterSignal.update((v) => v + 1);
+            console.log('Tick');
+        }, 2000);
+    }
 
     increaseBy(value: number) {
         this.counter += value;
